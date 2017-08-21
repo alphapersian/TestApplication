877c95901f10bb0d6845d7e3e7a664e84bf15f45
ahmad <ahmademosavi@gmail.com>
2017-08-21 21:08:10 +0430

    merged with google sample

--- app/src/main/java/com/example/root/testapplication/vpn/Main4Activity.java
+++ app/src/main/java/com/example/root/testapplication/vpn/Main4Activity.java
@@ -1,25 +1,43 @@
 package com.example.root.testapplication.vpn;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.VpnService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.root.testapplication.R;
 
 public class Main4Activity extends AppCompatActivity {
-
-    private static final String TAG = "Main4Activity";
+    public interface Prefs {
+        String NAME = "connection";
+        String SERVER_ADDRESS = "server.address";
+        String SERVER_PORT = "server.port";
+        String SHARED_SECRET = "shared.secret";
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        final TextView serverAddress = (TextView) findViewById(R.id.address);
+        final TextView serverPort = (TextView) findViewById(R.id.port);
+        final TextView sharedSecret = (TextView) findViewById(R.id.secret);
+        final SharedPreferences prefs = getSharedPreferences(Prefs.NAME, MODE_PRIVATE);
+        serverAddress.setText(prefs.getString(Prefs.SERVER_ADDRESS, ""));
+        serverPort.setText(prefs.getString(Prefs.SERVER_PORT, ""));
+        sharedSecret.setText(prefs.getString(Prefs.SHARED_SECRET, ""));
         findViewById(R.id.connect).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = VpnService.prepare(getApplicationContext());
+                prefs.edit()
+                        .putString(Prefs.SERVER_ADDRESS, serverAddress.getText().toString())
+                        .putString(Prefs.SERVER_PORT, serverPort.getText().toString())
+                        .putString(Prefs.SHARED_SECRET, sharedSecret.getText().toString())
+                        .commit();
+                Intent intent = VpnService.prepare(Main4Activity.this);
                 if (intent != null) {
                     startActivityForResult(intent, 0);
                 } else {
@@ -27,16 +45,20 @@ public class Main4Activity extends AppCompatActivity {
                 }
             }
         });
-        
-        
+        findViewById(R.id.disconnect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(getServiceIntent().setAction(MyVpnService.ACTION_DISCONNECT));
+            }
+        });
     }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (resultCode == RESULT_OK) {
-            Intent intent = new Intent(this, MyVpnService.class);
-            startService(intent);
+    protected void onActivityResult(int request, int result, Intent data) {
+        if (result == RESULT_OK) {
+            startService(getServiceIntent().setAction(MyVpnService.ACTION_CONNECT));
         }
+    }
+    private Intent getServiceIntent() {
+        return new Intent(this, MyVpnService.class);
     }
 }
